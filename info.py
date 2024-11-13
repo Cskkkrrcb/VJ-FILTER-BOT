61b8d6d30fe3f0731e3f2efac9f0f8988bc77373
SK-RAMIJ-RAJA <akabusiness010@gmail.com>
2024-11-13 21:26:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Yq4IVa30_YNkMjY1')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+4Bm5TskV3P9lYTM1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/LuxMovies_vip')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Verification_vip')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Movie_Night100') # Support Chat Link Without https:// or @
